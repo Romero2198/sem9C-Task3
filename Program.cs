e328a781c71d5d49ed31ca730f741d34a4b93d00
Yung.Energy <morskojroma@gmail.com>
2022-08-07 14:04:21 +0300

    Task3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число M: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine($"M = {m}; N = {n} -> A(m,n) = " + AckermanFunction(m, n));
+
+int AckermanFunction(int m, int n)
+{
+    if (m == 0)
+        return n++;
+    if (n == 0)
+        return AckermanFunction(m - 1, 1);
+    if (m > 0 && n > 0)
+    {
+        return AckermanFunction(m - 1, AckermanFunction(m, n - 1));
+    }
+    return AckermanFunction(m, n);
+}
